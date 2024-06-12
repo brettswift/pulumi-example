eabd2d3743708dba96f3661a1fc51eef38a3093f
Brett Swift <brett.swift@caylent.com>
2024-06-12 13:24:59 -0600

    build with docker

--- package_lambdas.sh
+++ package_lambdas.sh
@@ -1,22 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cd "$(dirname "$0")" || exit 1
 
 build_lambda() {
-  echo "Building $1"
-  cd lambdas/"$1" || exit 1
-  rm -rf ./publish
-  mkdir -p ../publish
-  cp -r . ../publish/
-  python3 -m venv venv
-  source venv/bin/activate
-  pip install -r requirements.txt -t ../publish/
-  mv ../publish ./publish
-  cd publish || exit 1
-  zip -r ../package.zip . 
-  cd ..
-  rm -rf ./publish/*
-  mv package.zip ./publish/
+    echo "Building $1"
+    cd lambdas/"$1" || exit 1
+    rm -rf ./publish
+    mkdir -p ./publish
+    rsync -av --progress . ./publish/ --exclude publish
+    
+    # Build the Docker image
+    docker build -t lambda-package .
+
+    # Run the Docker image and copy the zip file from the container to the host
+    docker run --name lambda-container lambda-package
+    docker cp lambda-container:/app/package.zip ./publish/
+
+    # Clean up the Docker container
+    docker rm lambda-container
+
+    # Clean up the Docker image
+    # docker rmi lambda-package
+
+    cd ..
 }
 
 build_lambda "queue_processor"
